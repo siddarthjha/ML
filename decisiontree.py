c0880fcea161102c4acd83499606eb2666dea894
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-11 11:57:01 +0530

    Add files via upload

---
+++ decisiontree.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 13 21:12:54 2015
+
+@author: ldierker
+"""
+
+# -*- coding: utf-8 -*-
+
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pylab as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+import sklearn.metrics
+
+
+"""
+Data Engineering and Analysis
+"""
+#Load the dataset
+
+AH_data = pd.read_csv("Decision tree.csv")
+
+data_clean = AH_data.dropna()
+print('Hello')
+
+data_clean.dtypes
+data_clean.describe()
+
+
+"""
+Modeling and Prediction
+"""
+#Split into training and testing sets
+
+predictors = data_clean[['BIO_SEX','HISPANIC','WHITE','BLACK','NAMERICAN','ASIAN',
+'age','ALCEVR1','ALCPROBS1','marever1','cocever1','inhever1','cigavail','DEP1',
+'ESTEEM1','VIOL1','PASSIST','DEVIANT1','SCHCONN1','GPA1','EXPEL1','FAMCONCT','PARACTV',
+'PARPRES']]
+
+targets = data_clean.TREG1
+
+pred_train, pred_test, tar_train, tar_test  =   train_test_split(predictors, targets, test_size=.4)
+
+print(pred_train.shape)
+print(pred_test.shape)
+print(tar_train.shape)
+print(tar_test.shape)
+
+#Build model on training data
+classifier=DecisionTreeClassifier()
+classifier=classifier.fit(pred_train,tar_train)
+print(classifier)
+predictions=classifier.predict(pred_test)
+print(predictions)
+
+print(sklearn.metrics.confusion_matrix(tar_test,predictions))
+print(sklearn.metrics.accuracy_score(tar_test, predictions))
+
+
+
+
+
+
