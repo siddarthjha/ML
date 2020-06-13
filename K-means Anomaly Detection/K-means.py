e1cf3b3fecd527c7567670a6cd3af693c7e5a156
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-13 13:06:16 +0530

    Update K-means.py

--- K-means Anomaly Detection/K-means.py
+++ K-means Anomaly Detection/K-means.py
@@ -1,1 +1,15 @@
+# Libraries
+
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+
+# Extract data
+df = pd.read_csv('Sample.csv')
+print(df.head())
+
+# Understand Data
 
