f34834a1db7568844d5926eadcde6aa51e1d7dcc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-15 08:23:31 +0530

    Update FA.py

--- Factor Analysis/FA.py
+++ Factor Analysis/FA.py
@@ -26,3 +26,31 @@ print(chi_square_value, p_value)
 fa = FactorAnalyzer()
 fa.set_params(n_factors=25, rotation = None)
 fa.fit(df)
+# Check Eigenvalues
+ev, v = fa.get_eigenvalues()
+print(ev)
+
+# Here, you can see only for 6-factors eigenvalues are greater than one. It means we need to choose only 6 factors (or unobserved variables).
+
+# Create scree plot using matplotlib
+plt.scatter(range(1,df.shape[1]+1),ev)
+plt.plot(range(1,df.shape[1]+1),ev)
+plt.title('Scree Plot')
+plt.xlabel('Factors')
+plt.ylabel('Eigenvalue')
+plt.grid()
+plt.show()
+
+# Create factor analysis object and perform factor analysis
+fa = FactorAnalyzer()
+fa.set_params(n_factors=6, rotation = None)
+fa.fit(df)
+print(fa.loadings_)
+# Create factor analysis object and perform factor analysis using 5 factors
+fa = FactorAnalyzer()
+fa.set_params(n_factors=5, rotation = None)
+print(fa.loadings_)
+
+# Get variance of each factors
+
+print(fa.get_factor_variance())
