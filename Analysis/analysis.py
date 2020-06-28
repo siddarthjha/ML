63ecaab609d1e17af4836d729ac9149f4c60381d
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-28 10:56:11 +0530

    Update analysis.py

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -9,3 +9,13 @@ def Nan_percent(df, column_name):            # Function to check the % of null v
 	row_count = df[column_name].shape[0]
 	empty_values = row_count - df[column_name].count()
 	return (100.0 * empty_values) / row_count
+
+
+d = pd.read_csv('athlete_events.csv')                  # Loads the data
+print('The number of rows and columns', d.shape)
+data = pd.DataFrame(d)                                 # The dataframe of the dataset is created
+print(data.info())
+print(data.describe(include = 'all'))
+print(data.dtypes)
+
+
