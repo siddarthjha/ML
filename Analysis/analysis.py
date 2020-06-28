600c1f46c3e4a32c5dc1e6cbe79b779b98b0f684
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-28 10:54:59 +0530

    Update analysis.py

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,1 +1,11 @@
+import pandas as pd                          # Pandas module for data analysis
+import numpy as np                           # Numpy module for mathematical calculations
+from matplotlib import pyplot as plt         # Pyplot module for data plotting
+import seaborn as sns                        # Seaborn module for data visualization
+from collections import Counter              # Counter module for count of key value pairs
+# Importing all the required libraries
 
+def Nan_percent(df, column_name):            # Function to check the % of null values present in each column 
+	row_count = df[column_name].shape[0]
+	empty_values = row_count - df[column_name].count()
+	return (100.0 * empty_values) / row_count
