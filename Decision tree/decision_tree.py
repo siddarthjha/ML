6f5433fa89e80aaa242c11db62c0bc31bd7a1035
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-28 11:21:44 +0530

    Update decision_tree.py

--- Decision tree/decision_tree.py
+++ Decision tree/decision_tree.py
@@ -1,1 +1,11 @@
 import pandas as pd
+from pandas import Series, DataFrame
+import pandas as pd
+import numpy as np
+import os
+import matplotlib.pylab as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import classification_report
+import sklearn.metrics
+
