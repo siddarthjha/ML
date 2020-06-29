1ebdfd67f020d78e561f077f9b3e2a77565751f4
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-29 18:21:38 +0530

    Update pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -13,9 +13,7 @@ X=data.values
 
 #Scaling the values
 X = scale(X)
-
 pca = PCA(n_components=44)
-
 pca.fit(X)
 
 #The amount of variance that each PC explains
@@ -23,11 +21,9 @@ var= pca.explained_variance_ratio_
 
 #Cumulative Variance explains
 var1=np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
-
 print(var1)
-
-
 plt.plot(var1)
+
 #Looking at above plot I'm taking 30 variables
 pca = PCA(n_components=30)
 pca.fit(X)
