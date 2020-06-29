fea2ffae6f67c91aa1e779c61b4da32f816c6039
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-29 13:01:04 +0530

    Update pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -1,1 +1,12 @@
+import numpy as np
+from sklearn.decomposition import PCA
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import scale
+%matplotlib inline
 
+#Load data set
+data = pd.read_csv('Big_Mart_PCA.csv')
+
+#convert it to numpy arrays
+X=data.values
