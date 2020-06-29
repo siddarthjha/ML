c35e20d0617a374aa026d0370bfd0a1d051b8d8f
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-29 18:01:32 +0530

    Update pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -10,3 +10,21 @@ data = pd.read_csv('Big_Mart_PCA.csv')
 
 #convert it to numpy arrays
 X=data.values
+
+#Scaling the values
+X = scale(X)
+
+pca = PCA(n_components=44)
+
+pca.fit(X)
+
+#The amount of variance that each PC explains
+var= pca.explained_variance_ratio_
+
+#Cumulative Variance explains
+var1=np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
+
+print var1
+
+
+plt.plot(var1)
