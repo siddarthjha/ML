467bb1351687257759379d118b367e1f9e12b7a1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-29 18:02:33 +0530

    Update pca.py

--- PCA/pca.py
+++ PCA/pca.py
@@ -24,7 +24,13 @@ var= pca.explained_variance_ratio_
 #Cumulative Variance explains
 var1=np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
 
-print var1
+print(var1)
 
 
 plt.plot(var1)
+#Looking at above plot I'm taking 30 variables
+pca = PCA(n_components=30)
+pca.fit(X)
+X1=pca.fit_transform(X)
+
+print(X1)
